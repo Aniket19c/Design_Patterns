f30acaea93bac1ea149b2b93a31cf333973403b3
Aniket19c <aniketchaudhary19j@gmail.com>
2025-03-25 18:58:33 +0530

    Implemented Strategy Design Pattern

--- Design_Patterns/Program.cs
+++ Design_Patterns/Program.cs
@@ -6,10 +6,73 @@ using System.Threading.Tasks;
  
 namespace Design_Patterns
 {
-    internal class Program
+    using System;
+
+    interface IPaymentStrategy
     {
-        static void Main(string[] args)
+        void Pay(int amount);
+    }
+
+    class CreditCardPayment : IPaymentStrategy
+    {
+        public void Pay(int amount)
         {
+            Console.WriteLine($"Paid {amount} using Credit Card.");
         }
     }
+
+    class PayPalPayment : IPaymentStrategy
+    {
+        public void Pay(int amount)
+        {
+            Console.WriteLine($"Paid {amount} using PayPal.");
+        }
+    }
+
+    class BitcoinPayment : IPaymentStrategy
+    {
+        public void Pay(int amount)
+        {
+            Console.WriteLine($"Paid {amount} using Bitcoin.");
+        }
+    }
+
+    class ShoppingCart
+    {
+        private IPaymentStrategy _paymentStrategy;
+
+        public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void Checkout(int amount)
+        {
+            if (_paymentStrategy == null)
+            {
+                Console.WriteLine("Please select a payment method before checking out.");
+                return;
+            }
+            _paymentStrategy.Pay(amount);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ShoppingCart cart = new ShoppingCart();
+
+            cart.SetPaymentStrategy(new CreditCardPayment());
+            cart.Checkout(500);
+
+            cart.SetPaymentStrategy(new PayPalPayment());
+            cart.Checkout(300);
+
+            cart.SetPaymentStrategy(new BitcoinPayment());
+            cart.Checkout(200);
+            Console.ReadLine();
+        }
+    }
+
 }
