0151f9a891ee1ac3f509365a25958939b21d7722
Aniket19c <aniketchaudhary19j@gmail.com>
2025-03-25 18:46:11 +0530

    Implemented Bridge Design Pattern

--- Design_Patterns/Program.cs
+++ Design_Patterns/Program.cs
@@ -6,10 +6,73 @@ using System.Threading.Tasks;
  
 namespace Design_Patterns
 {
-    internal class Program
+
+    interface IColor
     {
-        static void Main(string[] args)
+        void ApplyColor();
+    }
+
+    class RedColor : IColor
+    {
+        public void ApplyColor()
         {
+            Console.WriteLine("Applying Red Color");
         }
     }
+
+    class BlueColor : IColor
+    {
+        public void ApplyColor()
+        {
+            Console.WriteLine("Applying Blue Color");
+        }
+    }
+
+    abstract class Shape
+    {
+        protected IColor color;
+
+        protected Shape(IColor color)
+        {
+            this.color = color;
+        }
+
+        public abstract void Draw();
+    }
+
+    class Circle : Shape
+    {
+        public Circle(IColor color) : base(color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Drawing Circle with ");
+            color.ApplyColor();
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square(IColor color) : base(color) { }
+
+        public override void Draw()
+        {
+            Console.Write("Drawing Square with ");
+            color.ApplyColor();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Shape redCircle = new Circle(new RedColor());
+            redCircle.Draw();
+
+            Shape blueSquare = new Square(new BlueColor());
+            blueSquare.Draw();
+            Console.ReadLine();
+        }
+    }
+
 }
